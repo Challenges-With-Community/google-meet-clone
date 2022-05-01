a2307b6db2cffb780ed88876e0cb717cf1cc1847
Santiago Restrepo <santiagorv246@gmail.com>
2022-05-01 01:55:18 -0500

    Pagina del home

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,14 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Button from "@material-ui/core/Button";
-import { green } from '@material-ui/core/colors';
-import { AddCircle } from "@material-ui/icons";
+import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <Button variant="contained" color="primary" type="button" onClick={() => setCount((count) => count + 1)}>
-            <AddCircle style={{ color: green[500], marginRight: "10px" }} />
-            count is: {count}
-          </Button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Header />
+      <Home />
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,107 @@
-import React from "react";
+import React, {useState} from "react";
+import NewMeeting from "../components/NewMeeting";
+import {Button, Box, TextField, InputAdornment, Divider} from "@material-ui/core";
+import Carousel from "../components/Carousel";
+import KeyboardOutlinedIcon from '@material-ui/icons/KeyboardOutlined';
 
 const Home = () => {
-    return(
-        <div>
-            Home
+    const [isDisabled, setIsDisabled] = useState(true);
+    const [roomCode, setRoomCode] = useState('');
+    const [joinButtonClass, setJoinButtonClass] = useState('Hide-Join-Button');
+
+    const handleFocus = (e) => {
+        e.preventDefault();
+        setJoinButtonClass('Show-Join-Button');
+    }
+
+    const handleBlur = (e) => {
+        e.preventDefault();
+        if (roomCode.length > 0) {
+            setJoinButtonClass('Show-Join-Button');
+        } else {
+            setJoinButtonClass('Hide-Join-Button');
+        }
+    }
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        const value = e.target.value;
+        setRoomCode(value);
+        if (value.length === 0) {
+            setIsDisabled(true)
+        } else {
+            setIsDisabled(false)
+        }
+    }
+
+    return (
+        <div className="Home">
+            <div className="Home-Meeting">
+                <div className="Home-Meeting-Description">
+                    <h1>Videoconferencias seguras para todos</h1>
+                    <p className="GoogleMeetDescription">Conecta, colabora y celebra desde cualquier lugar con Google Meet</p>
+                </div>
+                <div className="Home-Meeting-Buttons">
+                    <NewMeeting />
+                    <div className="Home-Meeting-Buttons-Code">
+                        <TextField
+                            onFocus={handleFocus}
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            type="text"
+                            size="medium"
+                            id="input-rom-code"
+                            value={roomCode}
+                            variant="outlined"
+                            placeholder="Introduce un código o apodo"
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <KeyboardOutlinedIcon />
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                        <Button
+                            color="primary"
+                            id="join-button"
+                            className={joinButtonClass}
+                            style={{
+                                height: '3em',
+                                fontSize: '1em',
+                                marginBottom: '1em',
+                                marginRight: '.5em',
+                                marginLeft: '.5rem',
+                                whiteSpace: 'nowrap',
+                                outline: '1px solid transparent',
+                                textTransform: 'none',
+                            }}
+                            disabled={isDisabled}
+                        >
+                            Unirme
+                        </Button>
+                    </div>
+                    <div className="LinkContainerOne">
+                        <div style={{
+                            marginTop: '2em',
+                            borderBottom: '1px solid',
+                            alignSelf: 'stretch',
+                            width: '100%',
+                            borderColor: '#dadce0',
+                        }} />
+                        <div style={{ marginTop: '2rem' }}>
+                            <a className="Link" href="https://meet.google.com/about/redirect/landing-learn-more/?hl=es" target="_blank">Más información sobre Google Meet</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Divider variant="middle" />
+            <div className="Home-Info">
+                <Carousel />
+                <div className="LinkContainerTwo">
+                    <a className="Link" href="https://meet.google.com/about/redirect/landing-learn-more/?hl=es" target="_blank">Más información sobre Google Meet</a>
+                </div>
+            </div>
         </div>
     )
 }
