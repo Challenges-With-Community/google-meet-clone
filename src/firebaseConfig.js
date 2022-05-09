fe2611396635d74d5857b6055bb6c3b212132737
Jose Luis <jdesignermayor@gmail.com>
2022-05-09 12:07:42 -0500

    se agrega la config de firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDfKv_90UKeDm-z9oVAOKFwpbIPfSEJUuY",
+  authDomain: "meet-clone-d9de2.firebaseapp.com",
+  projectId: "meet-clone-d9de2",
+  storageBucket: "meet-clone-d9de2.appspot.com",
+  messagingSenderId: "375053355695",
+  appId: "1:375053355695:web:b34902ef37ff0194e4a25a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
