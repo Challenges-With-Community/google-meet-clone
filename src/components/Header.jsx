a4f052053094fd8abf7aa8d1999d2ef3d53aa5e8
Jose Luis <jdesignermayor@gmail.com>
2022-05-09 12:06:59 -0500

    se limpia el codigo y se refactoriza

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,73 +1,79 @@
-import React, {useEffect, useState} from 'react';
-import {Avatar} from "@material-ui/core";
-import FeedbackOutlinedIcon from '@material-ui/icons/FeedbackOutlined';
-import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
-import HelpOutlineOutlinedIcon from '@material-ui/icons/HelpOutlineOutlined';
+import { useHeader } from "../hooks/useHeader";
+
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import FeedbackOutlinedIcon from "@material-ui/icons/FeedbackOutlined";
+import HelpOutlineOutlinedIcon from "@material-ui/icons/HelpOutlineOutlined";
 import IconButton from "@material-ui/core/IconButton";
+import SettingsOutlinedIcon from "@material-ui/icons/SettingsOutlined";
 
 const Header = () => {
-    const [time, setTime] = useState('');
-
-    const days = ['dom', 'lun', 'mar', 'mie', 'jue', 'vie', 'sab'];
-    const months = ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic'];
-
-    const date = () => {
-        let today = new Date();
-        let month = months[today.getMonth()];
-        let dayNumber = today.getDate();
-        let day = days[today.getDay()];
-        let hour = today.getHours();
-        let minutes = today.getMinutes();
-        if (String(minutes).length === 1) {
-            minutes = `0${minutes}`;
-        }
-        return `${hour}:${minutes} • ${day}, ${dayNumber} ${month}`;
-    }
-
-    useEffect(() => {
-        setInterval(() => {
-            setTime(date());
-        }, 1000)
-    }, [])
+  const { user, signIn, isUserExist, currentDate} = useHeader();
 
   return (
-      <div className="Header">
-          <div className="Logo">
-              <a href="/" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none' }}>
-                  <img src='src/assets/img/google_meet_horizontal_wordmark_2020q4_1x_icon.png' alt='Google Meet Image'/>
-                  <span style={{
-                      paddingLeft: '4px',
-                      color: '#5f6368',
-                      opacity: 1,
-                      display: 'inline-block',
-                      fontFamily: '"Product Sans",Arial,sans-serif',
-                      fontSize: '22px',
-                      lineHeight: '24px',
-                      position: 'relative',
-                      top: '-1.5px',
-                      verticalAlign: 'middle',
-                      fontWeight: 400
-                  }}
-                  >
-                      Meet
-                  </span>
-              </a>
-          </div>
-              <div className="Header-Info">
-                  <div className="Datetime">
-                      {date()}
-                  </div>
-                  <div className="Header-Info-Icons">
-                      <IconButton><HelpOutlineOutlinedIcon /></IconButton>
-                      <IconButton><FeedbackOutlinedIcon /></IconButton>
-                      <IconButton><SettingsOutlinedIcon /></IconButton>
-                  </div>
-              </div>
-              <div>
-                  <Avatar alt="Profile image" src='' />
-              </div>
+    <div className="Header">
+      <div className="Logo">
+        <a
+          href="/"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            textDecoration: "none",
+          }}
+        >
+          <img
+            src="src/assets/img/google_meet_horizontal_wordmark_2020q4_1x_icon.png"
+            alt="Google Meet Image"
+          />
+          <span
+            style={{
+              paddingLeft: "4px",
+              color: "#5f6368",
+              opacity: 1,
+              display: "inline-block",
+              fontFamily: '"Product Sans",Arial,sans-serif',
+              fontSize: "22px",
+              lineHeight: "24px",
+              position: "relative",
+              top: "-1.5px",
+              verticalAlign: "middle",
+              fontWeight: 400,
+            }}
+          >
+            Meet
+          </span>
+        </a>
       </div>
+      <div className="Header-Info">
+        <div className="Datetime">{currentDate}</div>
+        <div className="Header-Info-Icons">
+          <IconButton>
+            <HelpOutlineOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <FeedbackOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsOutlinedIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div style={{ paddingLeft: "18px" }}>
+        {isUserExist ? (
+          <Avatar alt="Profile image" src={user?.photoURL} />
+        ) : (
+          <Button
+            color="primary"
+            disableElevation={true}
+            variant="contained"
+            onClick={signIn}
+          >
+            Iniciar sesion
+          </Button>
+        )}
+      </div>
+    </div>
   );
-}
+};
 
 export default Header;
